8abddae31fc93ad2acec0909c0fa25fc5b407319
Jonathan Velasquez <johedvelca@gmail.com>
2025-02-25 20:16:16 -0500

    Exit popup finished, working on user components connection with the GUI.

--- src/main/java/com/example/websocket_demo/client/ClientGUI.java
+++ src/main/java/com/example/websocket_demo/client/ClientGUI.java
@@ -3,6 +3,8 @@ package com.example.websocket_demo.client;
 import com.example.websocket_demo.Message;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.concurrent.ExecutionException;
 
 public class ClientGUI extends JFrame {
@@ -12,5 +14,14 @@ public class ClientGUI extends JFrame {
         setSize(1218, 685);
         setLocationRelativeTo(null); //loads the GUI in the center of screen
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int option = JOptionPane.showConfirmDialog(ClientGUI.this, "Do you really want to leave?", "Exit", JOptionPane.YES_NO_OPTION);
+                if (option == JOptionPane.YES_OPTION) {
+                    ClientGUI.this.dispose();
+                }
+            }
+        });
     }
 }
