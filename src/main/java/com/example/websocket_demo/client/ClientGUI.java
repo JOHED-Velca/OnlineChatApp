f817c0ef47e3cf4c5ae071e7efa7201a2f5e138c
Jonathan Velasquez <johedvelca@gmail.com>
2025-02-25 22:13:56 -0500

    client's chat GUI display v1 finished, working on its connection with the websocket.

--- src/main/java/com/example/websocket_demo/client/ClientGUI.java
+++ src/main/java/com/example/websocket_demo/client/ClientGUI.java
@@ -1,12 +1,16 @@
 package com.example.websocket_demo.client;
 
+import com.example.websocket_demo.Message;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class ClientGUI extends JFrame {
-    private JPanel connectedUsersPanel;
+    private JPanel connectedUsersPanel, messagePanel;
     public ClientGUI(String username) {
         super("User: " + username);
 
@@ -29,10 +33,12 @@ public class ClientGUI extends JFrame {
 
     private void addGuiComponents() {
         addConnectedUserComponents();
+        addChatComponents();
     }
 
     private void addConnectedUserComponents() {
         connectedUsersPanel = new JPanel();
+        connectedUsersPanel.setBorder(Utilities.addPadding(10, 10, 10, 10));
         connectedUsersPanel.setLayout(new BoxLayout(connectedUsersPanel, BoxLayout.Y_AXIS));
         connectedUsersPanel.setBackground(Utilities.SECONDARY_COLOR);
         connectedUsersPanel.setPreferredSize(new Dimension(200, getHeight()));
@@ -43,5 +49,73 @@ public class ClientGUI extends JFrame {
         connectedUsersPanel.add(connectedUsersLabel);
 
         add(connectedUsersPanel, BorderLayout.WEST);
+    }
+
+    private void addChatComponents() {
+        JPanel chatPanel = new JPanel(); //JPanel default layout manager is "flow"
+        chatPanel.setLayout(new BorderLayout());
+        chatPanel.setBackground(Utilities.TRANSPARENT_COLOR);
+
+        messagePanel = new JPanel();
+        messagePanel.setLayout(new BoxLayout(messagePanel, BoxLayout.Y_AXIS));
+        messagePanel.setBackground(Utilities.TRANSPARENT_COLOR);
+        chatPanel.add(messagePanel, BorderLayout.CENTER);
+
+//        JLabel message = new JLabel("Random Message");
+//        message.setFont(new Font("Inter", Font.BOLD, 18));
+//        message.setForeground(Utilities.TEXT_COLOR);
+//        messagePanel.add(message);
+        messagePanel.add(createChatMessageComponent(new Message("TapTap", "Hello World")));
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.setBorder(Utilities.addPadding(10, 10, 10, 10));
+        inputPanel.setLayout(new BorderLayout());
+        inputPanel.setBackground(Utilities.TRANSPARENT_COLOR);
+
+        JTextField inputField = new JTextField();
+        inputField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyChar() == KeyEvent.VK_ENTER) {
+                    String input = inputField.getText();
+
+                    //edge case: empty message (prevent empty messages)
+                    if (input.isEmpty()) return;
+
+                    inputField.setText("");
+
+                    messagePanel.add(createChatMessageComponent(new Message("TapTap", input)));
+                    repaint();
+                    revalidate();
+                }
+            }
+        });
+        inputField.setBackground(Utilities.SECONDARY_COLOR);
+        inputField.setForeground(Utilities.TEXT_COLOR);
+        inputField.setFont(new Font("Inter", Font.PLAIN, 16));
+        inputField.setPreferredSize(new Dimension(inputPanel.getWidth(), 50));
+        inputPanel.add(inputField, BorderLayout.CENTER); //if not provided the second parameter, then it goes to the center by default
+        chatPanel.add(inputPanel, BorderLayout.SOUTH);
+
+        add(chatPanel, BorderLayout.CENTER);
+    }
+
+    private JPanel createChatMessageComponent(Message message) {
+        JPanel chatMessage = new JPanel();
+        chatMessage.setBackground(Utilities.TRANSPARENT_COLOR);
+        chatMessage.setLayout(new BoxLayout(chatMessage, BoxLayout.Y_AXIS));
+        chatMessage.setBorder(Utilities.addPadding(20, 20, 20, 20));
+
+        JLabel usernameLabel = new JLabel(message.getUser());
+        usernameLabel.setFont(new Font("Inter", Font.BOLD, 18));
+        usernameLabel.setForeground(Utilities.TEXT_COLOR);
+        chatMessage.add(usernameLabel);
+
+        JLabel messageLabel = new JLabel(message.getMessage());
+        messageLabel.setFont(new Font("Inter", Font.PLAIN, 15));
+        messageLabel.setForeground(Utilities.TEXT_COLOR);
+        chatMessage.add(messageLabel);
+
+        return chatMessage;
     }
 }
